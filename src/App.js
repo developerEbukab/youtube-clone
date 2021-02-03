dc3af4a56dff8b8ec8e76690dbc9e296c95ace8a
ChukwuEbuka Beluolisa <developerebukab@gmail.com>
2021-02-03 03:29:39 -0500

    Created homepage division, added title and icon

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="AppContainer">
+      <div className="Header">
+        Header
+      </div>
+      <div className="Body">
+        <div className="Menus">
+          Menus
+        </div>
+        <div className="Content">
+          Content
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
